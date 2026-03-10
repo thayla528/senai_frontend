b357f285d103bf89fc8a8c7330750aedd0514d3e
senai_25_26 <thaylamoralles@gmail.com>
2026-03-10 08:21:52 -0300

    frontend

--- static/js/apis.js
+++ static/js/apis.js
@@ -15,3 +15,40 @@ function render_gato(dados){
     imgGato.style.display = "block"
     imgGato.src = urlImg
 }
+
+
+async function getCachorro(){
+    let resultado = await fetch("https://dog.ceo/api/breeds/image/random")
+    if (resultado.ok) {
+        let dados = await resultado.json()
+        render_cachorro(dados)
+    }
+}
+
+function render_cachorro(dados){
+    let urlImg = dados.message
+    const imgCachorro=document.getElementById('img-cachorro')
+    const iconCachorro=document.getElementById('icon-cachorro')
+
+    iconCachorro.style.display = "none"
+    imgCachorro.style.display = "block"
+    imgCachorro.src = urlImg
+}
+
+async function getRaposa(){
+    let resultado = await fetch("https://randomfox.ca/floof")
+    if (resultado.ok) {
+        let dados = await resultado.json()
+        renderRaposa(dados)
+    }
+}
+
+function renderRaposa(dados){
+    let urlImg = dados.image
+    const imgRaposa=document.getElementById('img-raposa')
+    const iconRaposa=document.getElementById('icon-raposa')
+
+    iconRaposa.style.display = "none"
+    imgRaposa.style.display = "block"
+    imgRaposa.src = urlImg
+}
